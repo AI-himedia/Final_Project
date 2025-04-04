496916b7051d531ec9cf53a46ed143c0392fbe40
hyeonjin <jinnni919191@gmail.com>
2025-04-04 16:29:56 +0900

    [수정] 화자분리 CLOVA Speech API 테스트 파일 추가

--- python/tts/multiple_test.py
+++ python/tts/multiple_test.py
@@ -11,17 +11,34 @@ import torchaudio
 # 모델 직접 초기화 및 체크포인트를 로드
 # 세부 설정 직접 제어 및 특정 기능 추가/수정 가능
 
+# 폴더 생성
+def create_folders():
+    folders = ["input", "output", "final"]
+    for folder in folders:
+        os.makedirs(folder, exist_ok=True)
+
+create_folders()  # 폴더 생성
+
 # 모델 로드
 def load_model(model_name):
     ModelManager().download_model(model_name)
 
     model_path = os.path.join(get_user_data_dir("tts"), model_name.replace("/", "--"))
 
+    # XttsArgs
+    model_args = XttsArgs(
+        gpt_layers=40,  # 레이어 수 증가
+        gpt_n_model_channels=2048,  # 채널 수 증가
+        gpt_n_heads=32,  # 헤드 수 증가
+        gpt_use_perceiver_resampler=True  # 오디오 처리 능력 향상
+    )
+
     # XttsConfig
     config = XttsConfig(
         temperature=0.7,  # 창의성을 높이기 위해
         repetition_penalty=6.0,  # 반복을 줄이기 위해
         sound_norm_refs=True,  # 오디오 정규화
+        model_args=model_args   # 모델 아키텍처
     )
     config.load_json(os.path.join(model_path, "config.json"))
 
@@ -85,12 +102,26 @@ def inference(model, text, gpt_cond_latent, speaker_embedding):
     return res
 
 # 예시 사용
-speaker_wavs = [
-]  # 여러 음성 파일 경로
+# speaker_wavs = [
+#     "output/call_speaker_1_3.wav",
+#     "output/call_speaker_1_4.wav",
+#     "output/call_speaker_1_5.wav",
+#     "output/call_speaker_1_9.wav"
+# ]  # 여러 음성 파일 경로
+
+# 폴더 내의 모든 WAV 파일 경로를 리스트로 불러오기
+folder_name = "output"
+speaker_wavs = [os.path.join(folder_name, file) for file in os.listdir(folder_name) if file.endswith('.wav')]
+
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name, exist_ok=True)
+
+if not speaker_wavs:
+    print(f"[WARNING] No WAV files found in '{folder_name}' folder.")
 
 gpt_cond_latent, speaker_embedding = compute_speaker_latents(model, speaker_wavs)
 
-text = ""
+text = "" # 텍스트
 audio_tensor = inference(model, text, gpt_cond_latent, speaker_embedding)
 
 if audio_tensor is not None:
