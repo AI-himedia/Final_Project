29a3a4d9a38b849007c1ebc21112f61ee8b683ef
hyeonjin <jinnni919191@gmail.com>
2025-04-11 20:22:29 +0900

    [추가] SpringBoot 오디오 파일 FastAPI 전달 코드 추가

--- springboot/src/main/java/com/aix/againhello/call/dto/SaveResponseDTO.java
+++ springboot/src/main/java/com/aix/againhello/call/dto/SaveResponseDTO.java
@@ -1,31 +1,16 @@
 package com.aix.againhello.call.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SaveResponseDTO {
+
     private String status;
     private String message;
     private String uploadedFile;
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getUploadedFile() {
-        return uploadedFile;
-    }
-
-    public void setUploadedFile(String uploadedFile) {
-        this.uploadedFile = uploadedFile;
-    }
 }
