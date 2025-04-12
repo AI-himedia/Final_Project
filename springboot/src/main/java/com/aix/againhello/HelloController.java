0a22bb1a96db824bc646cf7553b6b4d1a190b6be
dijeungi <dijeungi@naver.com>
2025-04-12 14:42:06 +0900

    [배포] 04.12 | 테스트 8차

--- springboot/src/main/java/com/aix/againhello/HelloController.java
+++ springboot/src/main/java/com/aix/againhello/HelloController.java
@@ -2,6 +2,7 @@
 
 package com.aix.againhello.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/be")
 public class HelloController {
 
+    @Value("${spring.profiles.active:default}")
+    private String profile;
+
+    @Value("${spring.datasource.url:NOT_SET}")
+    private String datasourceUrl;
+
+    @Value("${custom.env.example:undefined}")
+    private String customEnv;
+
     // GET /be/test 요청 처리
     @GetMapping("/test")
     public String hello() {
-        System.out.println("테스트");
+        System.out.println("테스트 요청 수신");
+        System.out.println("[LOG] Profile: " + profile);
+        System.out.println("[LOG] Datasource URL: " + datasourceUrl);
+        System.out.println("[LOG] Custom ENV: " + customEnv);
         return "hello";
     }
-
-
 }
