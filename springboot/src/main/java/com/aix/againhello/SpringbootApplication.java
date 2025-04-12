e4eb511a10868fa61952f8f9a9cfddfa626d3b1f
hyeonjin <jinnni919191@gmail.com>
2025-04-12 11:34:05 +0900

    [수정] SpringBoot -> Python FastAPI로 화자분리 wav 전달 코드 수정

--- springboot/src/main/java/com/aix/againhello/SpringbootApplication.java
+++ springboot/src/main/java/com/aix/againhello/SpringbootApplication.java
@@ -3,8 +3,12 @@ package com.aix.againhello;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.client.HttpClientAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        HttpClientAutoConfiguration.class,
+        // CodecsAutoConfiguration.class // 필요에 따라 이것도 제외 고려
+})
 @MapperScan({
         "com.aix.againhello.oauth.kakao.mapper",
         "com.aix.againhello.sms",
