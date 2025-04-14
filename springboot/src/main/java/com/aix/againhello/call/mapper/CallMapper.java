e61b2d21853b56f088a7d7cb9720797feed971ae
hyeonjin <jinnni919191@gmail.com>
2025-04-14 17:47:39 +0900

    [추가] 사용자별 전화 서비스 구독 고인 목록 및 최근 통화 시간 조회 API 추가

--- springboot/src/main/java/com/aix/againhello/call/mapper/CallMapper.java
+++ springboot/src/main/java/com/aix/againhello/call/mapper/CallMapper.java
@@ -1,8 +1,11 @@
 package com.aix.againhello.call.mapper;
 
+import com.aix.againhello.call.dto.CallDeceasedInfoDTO;
 import com.aix.againhello.common.DeceasedDataDTO;
 import com.aix.againhello.common.RawFileDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CallMapper {
@@ -10,5 +13,5 @@ public interface CallMapper {
     void insertDeceasedData(DeceasedDataDTO deceasedData);
     void insertRawFile(RawFileDTO rawFile);
     int updateDeceasedData(DeceasedDataDTO deceasedDataDto);
-
+    List<CallDeceasedInfoDTO> findDeceasedListForCallServiceByUser(int userCode);
 }
