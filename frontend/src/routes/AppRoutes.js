090c5ad9ce14ffd980d7d8b889decb424ff8d230
dijeungi <dijeungi@naver.com>
2025-04-16 02:25:32 +0900

    [수정] 로그인 시 Header 아이콘 유지 기능 완성

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,6 +1,6 @@
 // src/routes/AppRoutes.js
-
 import { Routes, Route, Navigate } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 
 import MainPage from '../pages/main/MainPage';
 import LoginPage from '../pages/auth/LoginPage';
@@ -18,7 +18,6 @@ import RealTimeAudioStream from '../test/RealTimeAudioStream';
 import TTSAudioPlayer from '../test/TTSAudioPlayer';
 import CallService from '../test/CallService';
 import ChatTestPage from '../test/ChatTestPage';
-import PrivateRoute from './PrivateRoute';
 
 export const AppRoutes = () => (
   <Routes>
@@ -26,54 +25,27 @@ export const AppRoutes = () => (
     <Route path="/login" element={<LoginPage />} />
     <Route path="/signup" element={<SignUpPage />} />
 
-    {/* Private Page */}
-
-    <Route
-      path="/service/terms/check"
-      element={
-        <PrivateRoute>
-          <ServiceCheck />
-        </PrivateRoute>
-      }
-    />
-    <Route
-      path="/service/terms/product"
-      element={
-        <PrivateRoute>
-          <ProductPage />
-        </PrivateRoute>
-      }
-    />
-    <Route
-      path="/service/terms/product/sms"
-      element={
-        <PrivateRoute>
-          <SmsPage />
-        </PrivateRoute>
-      }
-    />
-    <Route
-      path="/service/terms/product/call"
-      element={
-        <PrivateRoute>
-          <CallPage />
-        </PrivateRoute>
-      }
-    />
-    <Route
-      path="/service/check"
-      element={
-        <PrivateRoute>
-          <ServiceCheck />
-        </PrivateRoute>
-      }
-    />
-
-    {/* 리디렉션 */}
-
+    {/* 공개 접근 라우트 */}
     <Route path="/service" element={<ApplyPage />} />
     <Route path="/service/terms" element={<TermsOfServicePage />} />
 
+    {/* 인증 필요 라우트 */}
+    <Route element={<PrivateRoute />}>
+      <Route path="/service/terms/check" element={<ServiceCheck />} />
+      <Route path="/service/terms/product" element={<ProductPage />} />
+      <Route path="/service/terms/product/sms" element={<SmsPage />} />
+      <Route path="/service/terms/product/call" element={<CallPage />} />
+      <Route path="/service/check" element={<ServiceCheck />} />
+    </Route>
+
+    {/* 테스트 라우트 */}
+    <Route path="/test" element={<ConnectionTestPage />} />
+    <Route path="/wstest" element={<RealTimeAudioStream />} />
+    <Route path="/ttstest" element={<TTSAudioPlayer />} />
+    <Route path="/call" element={<CallService />} />
+    <Route path="/chattest" element={<ChatTestPage />} />
+
+    {/* 리디렉션 */}
     <Route
       path="/success"
       element={<Navigate to="/service/terms/product" replace />}
@@ -82,12 +54,5 @@ export const AppRoutes = () => (
       path="/fail"
       element={<Navigate to="/service/terms/product" replace />}
     />
-
-    {/* 공개 테스트용 라우트 */}
-    <Route path="/test" element={<ConnectionTestPage />} />
-    <Route path="/wstest" element={<RealTimeAudioStream />} />
-    <Route path="/ttstest" element={<TTSAudioPlayer />} />
-    <Route path="/call" element={<CallService />} />
-    <Route path="/chattest" element={<ChatTestPage />} />
   </Routes>
 );
