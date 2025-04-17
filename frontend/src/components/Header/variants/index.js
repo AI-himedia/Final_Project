f94980b191f2e4d3d6fed10d2e6b3daaa00bc9f5
dijeungi <dijeungi@naver.com>
2025-04-17 11:04:56 +0900

    [추가] 고인 프로필 작성 Step1 완성

--- frontend/src/components/Header/variants/index.js
+++ frontend/src/components/Header/variants/index.js
@@ -3,3 +3,4 @@ export { default as HeaderTerms } from './HeaderTerms';
 export { default as HeaderProduct } from './HeaderProduct';
 export { default as HeaderApply } from './HeaderApply';
 export { default as HeaderCheck } from './HeaderCheck';
+export { default as HeaderDeceased } from './HeaderDeceased';
