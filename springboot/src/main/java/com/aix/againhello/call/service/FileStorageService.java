fe99950bdf196b0a0e0569bb2222eccc65b89c1a
hyeonjin <jinnni919191@gmail.com>
2025-04-22 20:49:47 +0900

    [수정] 필요없는 주석 삭제

--- springboot/src/main/java/com/aix/againhello/call/service/FileStorageService.java
+++ springboot/src/main/java/com/aix/againhello/call/service/FileStorageService.java
@@ -78,38 +78,6 @@ public class FileStorageService {
         }
     }
 
-//    public String storeFile(MultipartFile file, String type, int deceasedCode) {
-//        try {
-//            // 파일 확장자 확인
-//            String originalFilename = file.getOriginalFilename();
-//            if (originalFilename == null) {
-//                throw new RuntimeException("파일명이 없습니다.");
-//            }
-//
-//            String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase();
-//            List<String> audioExtList = Arrays.asList(allowedAudioExtensions.split(","));
-//            List<String> videoExtList = Arrays.asList(allowedVideoExtensions.split(","));
-//
-//            // 파일 저장 디렉토리 생성
-//            Path uploadPath = Paths.get(uploadDir);
-//            if (!Files.exists(uploadPath)) {
-//                Files.createDirectories(uploadPath);
-//            }
-//
-//            // 파일명 생성
-//            String newFilename = deceasedCode + "_" + originalFilename;
-//
-//            Path targetLocation = uploadPath.resolve(newFilename);
-//
-//            // 파일 저장 (기존 같은 파일이 있으면 덮어쓰기)
-//            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-//
-//            return targetLocation.toString();
-//        } catch (IOException ex) {
-//            throw new RuntimeException("파일 저장 중 오류가 발생했습니다: " + ex.getMessage());
-//        }
-//    }
-
     public String storeFile(MultipartFile file, String type, int subscriptionCode, int deceasedCode) {
         try {
             String originalFilename = file.getOriginalFilename();
